37ea2aa246a435184e98ad912d4d1f829a41fe14
Leon Forde <leonforde1201@gmail.com>
2026-02-27 20:37:48 +0000

    Add HestonSurfaceDataset class for data handling

---
+++ src/DatasetClass.py
@@ -0,0 +1,10 @@
+class HestonSurfaceDataset(Dataset):
+    def __init__(self, X: np.ndarray, y: np.ndarray):
+        self.X = torch.from_numpy(X).float()
+        self.y = torch.from_numpy(y).float()
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
